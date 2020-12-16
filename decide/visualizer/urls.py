2f586dcfe9f81461b82c84ce5cbd477e3ca7dc93
joscorfis <joscorfis@alum.us.es>
2020-12-16 22:07:44 +0100

    TASK-024: CSS+ HTML base + webresponsive + Modo nocturno - joscorfis - Funcionalidad

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import VisualizerView
+from .views import VisualizerView, Prueba
 
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
+    path('prueba/', Prueba.as_view()),
+   
 ]
