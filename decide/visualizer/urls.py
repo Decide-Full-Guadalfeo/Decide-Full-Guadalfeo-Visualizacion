ebd5a8b8aaa065f9624b79eff0f4fdfa0ec6eaaa
reyblacua <reyblacua@alum.us.es>
2020-12-28 18:29:34 +0100

    TASK-033: Configurar bot de Telegram en Decide - Reyes - Funcionalidad

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import VisualizerView, Prueba
-
+from visualizer import views
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
     path('prueba/', Prueba.as_view()),
-   
+    path('botResults/',views.botResponse),
 ]
