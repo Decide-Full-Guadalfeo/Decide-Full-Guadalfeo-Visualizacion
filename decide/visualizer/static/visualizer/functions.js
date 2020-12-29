4e8553f164fd296aa7d4193127ad9e1598a462c2
jossalase <56022937+jossalase@users.noreply.github.com>
2020-12-29 18:20:08 +0100

    Task-028: Implementación Modo Nocturno - José- funcionalidad

---
+++ decide/visualizer/static/visualizer/functions.js
@@ -0,0 +1,11 @@
+
+
+function cambiarModo(checkBox){
+    var cuerpoweb = document.body
+    if(checkBox){
+        cuerpoweb.classList.toggle("oscuro")
+    }
+    else if(!checkBox){
+        cuerpoweb.classList.toggle("claro")
+    }
+}
