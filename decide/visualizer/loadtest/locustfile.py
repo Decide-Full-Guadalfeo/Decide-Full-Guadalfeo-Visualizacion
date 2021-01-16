f607be7ef0c8bcdfae8b5cf4b3fc3ae4170f38f6
Pabcargam <pabcargam@alum.us.es>
2021-01-16 23:25:46 +0100

    Task-205: Pruebas de carga Locust Visualizer - Pablo - Prueba
    Se ha añadido y comprobado el funcionamiento del fichero "locust.py" satisfactoriamente

---
+++ decide/visualizer/loadtest/locustfile.py
@@ -0,0 +1,50 @@
+import json
+import requests
+
+from random import choice
+
+from locust import (
+    HttpUser,
+    SequentialTaskSet,
+    TaskSet,
+    task,
+    between
+)
+
+HOST = "http://localhost:8000"
+VOTING = 1
+
+class DefVisualizer(TaskSet):
+
+    @task
+    def index(self):
+        self.client.get("/visualizer/{0}/".format(VOTING))
+
+class DefAboutUs(TaskSet):
+
+    @task
+    def index(self):
+        self.client.get("/visualizer/aboutUs/")
+
+class DefContactUs(TaskSet):
+
+    @task
+    def index(self):
+        self.client.get("/visualizer/contactUs/")
+
+
+class Visualizer(HttpUser):
+    host = HOST
+    tasks = [DefVisualizer]
+    wait_time = between(3,5)
+
+class AboutUs(HttpUser):
+    host = HOST
+    tasks = [DefAboutUs]
+    wait_time = between(3,5)
+
+class ContactUs(HttpUser):
+    host = HOST
+    tasks = [DefContactUs]
+    wait_time = between(3,5)
+
